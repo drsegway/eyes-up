b75a12c75a80a2cd531a6332ac0358cbc4444b37
drsegway <89943043+drsegway@users.noreply.github.com>
2021-09-01 18:39:07 -0400

    Update MinimalGame.cs

--- code/MinimalGame.cs
+++ code/MinimalGame.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 //
 // You don't need to put things in a namespace, but it doesn't hurt.
-//
+// Don't know why we would do this lol - Seg
 
 
 
@@ -15,29 +15,25 @@ using System.Threading.Tasks;
 namespace MinimalExample
 {
 
-	/// <summary>
-	/// This is your game class. This is an entity that is created serverside when
+	
+	/// This is the game class. This is an entity that is created serverside when
 	/// the game starts, and is replicated to the client. 
-	/// 
-	/// You can use this to create things like HUDs and declare which player class
-	/// to use for spawned players.
-	/// </summary>
+	
+
+
 	public partial class MinimalGame : Sandbox.Game
 	{
 		public MyHUD MyHUD;
 		public MinimalGame()
 		{
-			if (IsClient) MyHUD = new MyHUD(); //creates new HUD
+			if (IsClient) MyHUD = new MyHUD(); //creates new HUD - Seg
 
 			if ( IsServer )
 			{
 				Log.Info( "My Gamemode Has Created Serverside!" );
 
-				// Create a HUD entity. This entity is globally networked
-				// and when it is created clientside it creates the actual
-				// UI panels. You don't have to create your HUD via an entity,
-				// this just feels like a nice neat way to do it.
-				new MinimalHudEntity();
+				
+				
 			}
 
 			if ( IsClient )
@@ -45,13 +41,20 @@ namespace MinimalExample
 				Log.Info( "My Gamemode Has Created Clientside!" );
 			}
 		}
-		
+		[Event.Hotload] // this refreshes the HUD every time a change is made. Only singleplayer? - Seg
+
+		public void HotLoadUpdate()
+		{
+
+			if ( !IsClient ) return;
+			MyHUD?.Delete();
+			MyHUD = new MyHUD();
+
+		}
 
 		
-
-		/// <summary>
-		/// A client has joined the server. Make them a pawn to play with
-		/// </summary>
+		/// A client has joined the server. Makes them a pawn to play with
+		
 		public override void ClientJoined( Client client )
 		{
 			base.ClientJoined( client );
