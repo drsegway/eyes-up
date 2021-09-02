fef27d9102b43907c22dc1bbe42445603d8c6559
drsegway <89943043+drsegway@users.noreply.github.com>
2021-09-02 14:26:21 -0400

    Update HealthUsable.cs

--- code/entities/HealthUsable.cs
+++ code/entities/HealthUsable.cs
@@ -5,11 +5,17 @@ public partial class HealthUsable : Prop, IUse
 	{
 	public override void Spawn()
 	{
+	
+	//spawns the crate, sets the model, and turns on physics.
+	
 		base.Spawn();
 
 		SetModel( "models/citizen_props/crate01.vmdl_c" );
 		SetupPhysicsFromModel( PhysicsMotionType.Static, false );
 
+
+	//makes the crate glow red.
+	
 		GlowState = GlowStates.GlowStateOn;
 		GlowDistanceStart = 0;
 		GlowDistanceEnd = 1000;
@@ -17,6 +23,8 @@ public partial class HealthUsable : Prop, IUse
 		GlowActive = true;
 
 	}
+	
+	// adds 50 health to the entity user, clamps health between 0 and 100.
 
 	public bool OnUse(Entity user)
 	{
@@ -26,7 +34,9 @@ public partial class HealthUsable : Prop, IUse
 
 		return false;
 	}
-
+	
+	// if health is > 100 you can not use the entity.
+	
 	public bool IsUsable(Entity user)
 	{
 		return user is Player ply && ply.Health < 100 ;
