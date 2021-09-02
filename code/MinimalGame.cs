be4781eaf5ccbf6b809af90325d0982be40aa255
drsegway <89943043+drsegway@users.noreply.github.com>
2021-09-01 20:15:22 -0400

    Add files via upload
    added cool gamer things

--- code/MinimalGame.cs
+++ code/MinimalGame.cs
@@ -15,41 +15,34 @@ using System.Threading.Tasks;
 namespace MinimalExample
 {
 
-	/// <summary>
-	/// This is your game class. This is an entity that is created serverside when
-	/// the game starts, and is replicated to the client. 
-	/// 
-	/// You can use this to create things like HUDs and declare which player class
-	/// to use for spawned players.
-	/// </summary>
+	
+
+	
 	public partial class MinimalGame : Sandbox.Game
 	{
+		
 		public MyHUD MyHUD;
 		public MinimalGame()
 		{
-			if (IsClient) MyHUD = new MyHUD(); //creates new HUD
+			
 
 			if ( IsServer )
 			{
 				Log.Info( "My Gamemode Has Created Serverside!" );
 
-				// Create a HUD entity. This entity is globally networked
-				// and when it is created clientside it creates the actual
-				// UI panels. You don't have to create your HUD via an entity,
-				// this just feels like a nice neat way to do it.
+			
 				
 			}
 
 			if ( IsClient )
 			{
 				Log.Info( "My Gamemode Has Created Clientside!" );
+				MyHUD = new MyHUD(); //creates new HUD
 			}
 		}
 		
 
-		/// <summary>
-		/// A client has joined the server. Make them a pawn to play with
-		/// </summary>
+		
 		public override void ClientJoined( Client client )
 		{
 			base.ClientJoined( client );
@@ -59,6 +52,11 @@ namespace MinimalExample
 
 			player.Respawn();
 		}
+
+		
+			
 	}
+
+	
 
 }
