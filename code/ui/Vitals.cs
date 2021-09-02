6c874003d8577dc376200a54b9fd12a1859620a3
drsegway <89943043+drsegway@users.noreply.github.com>
2021-09-02 13:40:16 -0400

    Switched out minimal gamemode
    Game no longer has framework from the minimal gamemode and will be much easier to modify.

--- code/ui/Vitals.cs
+++ code/ui/Vitals.cs
@@ -3,38 +3,39 @@ using Sandbox.UI;
 using Sandbox.UI.Construct;
 
 public partial class Vitals : Panel
-{   
-    private Label Health;
-    private Panel HealthBar;
-    public Vitals()
-    {
-        StyleSheet.Load("/ui/Vitals.scss");
+{
+	private Label Health;
+	private Panel HealthBar;
+	public Vitals()
+	{
+		StyleSheet.Load( "/ui/Vitals.scss" );
 
-        Panel healthBack = Add.Panel("healthBack");
+		Panel healthBack = Add.Panel( "healthBack" );
 
-        Panel healthBarBack = healthBack.Add.Panel ( "healthBarBack" );
-        HealthBar = healthBarBack.Add.Panel ("healthBar");
+		Panel healthBarBack = healthBack.Add.Panel( "healthBarBack" );
+		HealthBar = healthBarBack.Add.Panel( "healthBar" );
 
-        Health = healthBarBack.Add.Label("0", "healthText");
-
-
-    }
-    
-    public override void Tick()
-    {
-        var player = Local.Pawn;
-            if (player == null) return;
-
-            Health.Text = $"{player.Health.CeilToInt()}";
-
-            
-
-            HealthBar.Style.Dirty();
-            HealthBar.Style.Width = Length.Percent(player.Health); // really cool piece of code
+		Health = healthBarBack.Add.Label( "", "healthText" );
 
 
-        base.Tick();
-    }
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		var player = Local.Pawn;
+		if ( player == null ) return;
+
+		Health.Text = $"{player.Health.CeilToInt()}";
+
+
+
+		HealthBar.Style.Dirty();
+		HealthBar.Style.Width = Length.Percent( player.Health ); // really cool piece of code
+
+
+		
+	}
 }
 
-    
