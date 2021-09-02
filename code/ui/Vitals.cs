5b3c78795424c9f4f42efd634c9ef0b07ec48beb
drsegway <89943043+drsegway@users.noreply.github.com>
2021-09-01 20:16:11 -0400

    Add files via upload

--- code/ui/Vitals.cs
+++ code/ui/Vitals.cs
@@ -4,15 +4,37 @@ using Sandbox.UI.Construct;
 
 public partial class Vitals : Panel
 {   
+    private Label Health;
+    private Panel HealthBar;
     public Vitals()
     {
         StyleSheet.Load("/ui/Vitals.scss");
 
-        Add.Panel("healthBack");
+        Panel healthBack = Add.Panel("healthBack");
+
+        Panel healthBarBack = healthBack.Add.Panel ( "healthBarBack" );
+        HealthBar = healthBarBack.Add.Panel ("healthBar");
+
+        Health = healthBarBack.Add.Label("0", "healthText");
 
 
     }
     
+    public override void Tick()
+    {
+        var player = Local.Pawn;
+            if (player == null) return;
+
+            Health.Text = $"{player.Health.CeilToInt()}";
+
+            
+
+            HealthBar.Style.Dirty();
+            HealthBar.Style.Width = Length.Percent(player.Health); // really cool piece of code
+
+
+        base.Tick();
+    }
 }
 
     
